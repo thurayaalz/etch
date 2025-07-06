50ef9ce2959da4b36dfc11f8fd52b64ccf2877e1
thurayaalz <thuraya0alz@gmail.com>
2025-07-06 03:05:45 +0300

    made 16*16 grid

---
+++ etch.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+for (i=0;i<16;i++){
+    for (j=0;j<16;j++){
+        const div = document.createElement("div");
+        div.classList="pix";
+        container.appendChild(div);
+    }
+}
